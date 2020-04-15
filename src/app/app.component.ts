759cdfa2db17be2a6234f4c3c54c042a3bf976c6
tensa132 <thundermist84@gmail.com>
2020-04-15 10:18:13 +0700

    Genap Ganjil Print Jalan

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,39 @@ export class AppComponent  {
   name = 'Angular';
 
   hasil = 0;
+  print = "";
+  output = "";
   printPola(angkaPara, pangkatPara) {
     let angka = parseInt(angkaPara);
     let pangkat = parseInt(pangkatPara);
-    
     this.hasil = Math.pow(angka, pangkat);
-    alert(typeof(angka));
+    if(this.hasil % 2 == 0) {
+      this.print += "PolaB <br>";
+      for(let i = pangkat; i <= this.hasil; i++) {
+        for(let j = 0; j < i; j++) {
+          this.print += "*";
+        }
+        this.print += "<br>";
+      }
+      this.print += "<br>";
+      this.output = this.print;
+    }
+    else {
+      let coPangkat = pangkat;
+      this.print += "PolaA <br>";
+      for(let i = 0, coAngka = 1; i < pangkat; i++, coPangkat--, coAngka++) {
+        for(let j = 0; j < coPangkat; j++) {
+          this.print += "*";
+        }
+        if(coAngka <= angka) {
+          for(let k = 0; k < coAngka; k++) {
+            this.print += "O";
+          }
+        }
+        this.print += "<br>";
+      }
+      this.print += "<br>";
+      this.output = this.print;
+    }
   }
 }
