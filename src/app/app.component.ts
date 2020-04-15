5c958d2e98e7a8fdbbd6cce98a4f887f3c29d46f
tensa132 <thundermist84@gmail.com>
2020-04-15 09:07:22 +0700

    Design udah

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Angular';
+
+  printPola(angkaPara, pangkatPara) {
+    let angka = angkaPara;
+    let pangkat = pangkatPara;
+    
+  }
 }
